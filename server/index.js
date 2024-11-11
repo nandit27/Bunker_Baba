410f8e45bec2630965c8e919ec823e1da96459c1
Bhavya <bgprajapati575@gmail.com>
2024-11-11 22:55:51 +0530

    adding-backend-url

--- server/index.js
+++ server/index.js
@@ -4,14 +4,23 @@ const cors = require('cors');
 const multer = require('multer');
 
 const app = express();
-const port = 3001;
+const port = 3001 || process.env.PORT;
 
 // Global middleware
-app.use(cors(
-  {
-    origin: '*'
+const allowedOrigins = [
+  'http://localhost:5173',               // Local development
+  'https://bunker-baba-n3p2.vercel.app/',      // Deployed frontend on Vercel
+];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
   }
-));
+}));
 app.use(express.json());
 
 // Remove multer from here since it's already in the route
